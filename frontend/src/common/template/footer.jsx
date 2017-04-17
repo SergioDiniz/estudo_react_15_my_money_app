0bfc8012a5bc41dc3f74697b71cdf2dcb4634aea
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-17 13:01:58 -0300

    footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = (props) => {
+    return(
+        <footer className='main-footer'>
+            <div className='hidden-xs'>
+                <strong> Copyright &copy; 2017 <a href="#">Sérgio Diniz</a>.</strong> All rights reserved.
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
+
 const component = (props) => {
     return(
         <div className='wrapper'>
             <Header />
             <SideBar />
+            <div className='content-wrapper'>
+                <h1>Conteudo</h1>
+            </div>
+            <Footer />
         </div>
     )
 }
