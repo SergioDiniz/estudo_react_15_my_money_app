1388588d1203ec9e38919b86703947fed9893bba
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-17 11:02:54 -0300

    configurado webpack e importando coisas do admin-lte

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,12 @@
+import '../common/template/dependencias'
+import React from 'react'
+
+const component = (props) => {
+    return(
+        <div className='wrapper'>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default component
