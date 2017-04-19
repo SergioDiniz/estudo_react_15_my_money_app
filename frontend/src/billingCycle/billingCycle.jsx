9585b55e8622d6bc93adee8b19afeba100ff8523
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-19 09:22:39 -0300

    tab default

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,8 +9,14 @@ import TabsContent from '../common/tab/tabsContent'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
+import {selectTab} from '../common/tab/tabAction'
 
 class BillingCycle extends Component {
+
+    componentWillMount(){
+         this.props.selectTab('tabListar')
+    }
+
     render() {
         return (
             <div>
@@ -45,4 +53,8 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle;
+const mapDispatchToProps = (dispatch) => (
+    bindActionCreators({selectTab}, dispatch)
+)
+
+export default connect(null, mapDispatchToProps)(BillingCycle);
